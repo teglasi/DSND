e583df02f68616f6a5f61acbc5820ee9090ff64f
teglasi <42617199+teglasi@users.noreply.github.com>
2018-10-04 20:18:15 +0200

    Committed, working status.

--- Projects/2_Disaster_Response_Pipeline_project/models/train_classifier.py
+++ Projects/2_Disaster_Response_Pipeline_project/models/train_classifier.py
@@ -1,23 +1,24 @@
 # import libraries
 import sys
 import nltk
-nltk.download(['punkt', 'wordnet'])
+nltk.download(['punkt', 'wordnet', 'stopwords'])
+from nltk.corpus import stopwords
 import pandas as pd
-import numpy as np
 import sqlalchemy
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import classification_report
-from sklearn.model_selection import GridSearchCV
+import pickle
+import re
 
 def load_data(database_filepath):
     engine = sqlalchemy.create_engine('sqlite:///'+database_filepath)
-    df = pd.read_sql_table('CleanMessages', engine)
+    df = pd.read_sql_table('Messages', engine)
     df.drop(['original', 'genre'], axis=1, inplace=True)
     df.dropna(inplace=True)
     keep_labels = ['0', '1']
@@ -27,6 +28,7 @@ def load_data(database_filepath):
     return X, Y, df.loc[:, 'related':'direct_report'].columns
 
 def tokenize(text):
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
     clean_tokens = []
@@ -36,24 +38,34 @@ def tokenize(text):
     return clean_tokens
 
 def build_model():
+    stop_w = set(stopwords.words('english'))
     pipeline = Pipeline([
         ('features', FeatureUnion([
             ('text_pipeline', Pipeline([
-                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('vect', CountVectorizer(tokenizer=tokenize, lowercase=True, stop_words=stop_w)),
                 ('tfidf', TfidfTransformer())
             ]))
         ])),
-        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+        ('clf', MultiOutputClassifier(AdaBoostClassifier()))
     ])
+    pipeline_parameters = {
+        'features__text_pipeline__vect__ngram_range': (1, 2),
+        'features__text_pipeline__vect__max_df': 0.75,
+        'features__text_pipeline__vect__max_features': 5000,
+        'features__transformer_weights': {'text_pipeline': 0.5, 'starting_verb': 1},
+        'clf__estimator__n_estimators': 150,
+        'clf__estimator__learning_rate': 0.5
+    }
+    pipeline.set_params(**pipeline_parameters)
+    # print(pipeline.get_params(deep=False))
+    return pipeline
 
 def evaluate_model(model, X_test, Y_test, category_names):
-
-    pass
-
+    y_pred = model.predict(X_test)
+    print(classification_report(Y_test.astype(int), y_pred.astype(int)))
 
 def save_model(model, model_filepath):
-    pass
-
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 def main():
     if len(sys.argv) == 3:
