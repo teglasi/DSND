20f2f127d077c026729d5f6f024cb86bf9eb9839
Istvan Teglas <42617199+teglasi@users.noreply.github.com>
2019-01-23 17:27:53 +0100

    Add files via upload

--- Projects/3_Capstone_Project/arvatoutils.py
+++ Projects/3_Capstone_Project/arvatoutils.py
@@ -2,12 +2,15 @@
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.cluster import MiniBatchKMeans
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 rnd_state = 16
+point_size=100
+point_border=0.8
 
 def find_sparse_columns(dfs, min_missing_ratio):
     
@@ -133,17 +136,75 @@ def pca_results(good_data, pca):
 	variance_ratios.index = dimensions
 
 	# Create a bar plot visualization
-	fig, ax = plt.subplots(figsize = (14,8))
+	#fig, ax = plt.subplots(figsize = (14,8))
 
 	# Plot the feature weights as a function of the components
-	components.plot(ax = ax, kind = 'bar');
-	ax.set_ylabel("Feature Weights")
-	ax.set_xticklabels(dimensions, rotation=0)
+	#components.plot(ax = ax, kind = 'bar');
+	#ax.set_ylabel("Feature Weights")
+	#ax.set_xticklabels(dimensions, rotation=0)
 
 
 	# Display the explained variance ratios
-	for i, ev in enumerate(pca.explained_variance_ratio_):
-		ax.text(i-0.40, ax.get_ylim()[1] + 0.05, "Explained Variance\n          %.4f"%(ev))
+	#for i, ev in enumerate(pca.explained_variance_ratio_):
+	#	ax.text(i-0.40, ax.get_ylim()[1] + 0.05, "Explained Variance\n          %.4f"%(ev))
 
 	# Return a concatenated DataFrame
 	return pd.concat([variance_ratios, components], axis = 1)
+
+def feat_weights(pca, comp, feat_num, feat_names):
+    """Find features for a PCA component with the most and least weights.
+
+    Parameters
+    ----------
+    pca : Fitted PCA class
+    comp : int, the (sequence) number of the component
+    feat_num : int, the number of features to return
+    feat_names : list, the names of the features
+    
+    Output
+    ------
+    Dataframe of features with the most and least weights
+    """
+    frame = pd.DataFrame(pca.components_[comp], index=feat_names).sort_values(by=0, ascending=False)
+    feat_significant = pd.concat([frame.head(feat_num), frame.tail(feat_num)])
+    fig, ax = plt.subplots(figsize=(10,6))
+    ax.set_xlabel('Component weight')
+    ax.set_ylabel('Features')
+    ax.set_title('Feature weights for component {}.'.format(comp))
+    g1 = ax.barh(feat_significant.index, feat_significant[0])
+    plt.show()
+    return feat_significant
+
+
+def get_kmeans_score(data, center, batch_size):
+    kmeans = MiniBatchKMeans(n_clusters=center, batch_size=batch_size, random_state=rnd_state).fit(data)
+    model = kmeans.fit(data)
+    score = np.abs(model.score(data))
+    return score
+
+def plot_dataset(dataset, xlim=(-30, 40), ylim=(-15, 15)):
+    plt.figure(figsize=figsize)
+    plt.scatter(dataset[:,0], dataset[:,1], s=point_size, color="#00B3E9", edgecolor='black', lw=point_border)
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+    plt.show()
+
+def plot_clustered_dataset(dataset, y_pred, xlim=(-30, 40), ylim=(-15, 15), neighborhood=False, epsilon=0.5):
+    fig, ax = plt.subplots(figsize=figsize)
+
+    colors = np.array(list(islice(cycle(['#df8efd', '#78c465', '#ff8e34',
+                                         '#f65e97', '#a65628', '#984ea3',
+                                         '#999999', '#e41a1c', '#dede00']),
+                                  int(max(y_pred) + 1))))
+    colors = np.append(colors, '#BECBD6')
+
+    if neighborhood:
+        for point in dataset:
+            circle1 = plt.Circle(point, epsilon, color='#666666', fill=False, zorder=0, alpha=0.3)
+            ax.add_artist(circle1)
+
+    ax.scatter(dataset[:, 0], dataset[:, 1], s=point_size, color=colors[y_pred], zorder=10, edgecolor='black',
+               lw=point_border)
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+    plt.show()
